d7b720dc03e4e97af686843aa28b2f102a834aeb
Hammer Garita <36253181+HammerGarita@users.noreply.github.com>
2018-12-09 23:04:27 -0600

    Add files via upload

---
+++ example/js/scripts.js
@@ -0,0 +1,11 @@
+ScrollAnimate.add({
+  selector: '.animated',
+  animationClassName: 'fadeInLeft',
+  offset: '50%',
+  scroll: 'vertical',
+  triggerOnce: false,
+  disabled: ['tablet', 'mobile'],
+  delay: 0,
+  duration: '2000',
+  iterationCount: '1'
+})
