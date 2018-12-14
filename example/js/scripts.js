577bbce34d43d66d9e9ec8c25dcb52bbb915a4d7
Hammer Garita <36253181+HammerGarita@users.noreply.github.com>
2018-12-14 13:47:01 -0600

    Update scripts.js

--- example/js/scripts.js
+++ example/js/scripts.js
@@ -1,11 +1,24 @@
-ScrollAnimate.add({
-  selector: '.animated',
-  animationClassName: 'fadeInLeft',
-  offset: '50%',
-  scroll: 'vertical',
-  triggerOnce: false,
-  disabled: ['tablet', 'mobile'],
-  delay: 0,
-  duration: '2000',
-  iterationCount: '1'
-})
+(function () {
+  // let element = document.querySelector('.scroll-horizontal-container')
+  // element.addEventListener('wheel', (e) => {
+  //   if (e.deltaY !== 0) {
+  //     e.preventDefault()
+  //     window.requestAnimationFrame(() => {
+  //       element.scrollLeft += e.deltaY
+  //     })
+  //   }
+  // })
+
+  ScrollAnimate.add({
+    selector: '.animated',
+    animationClassName: 'fadeInUp',
+    offset: '10%',
+    scroll: 'vertical',
+    triggerOnce: false,
+    disabled: ['tablet', 'mobile'],
+    delay: 0,
+    duration: '1000',
+    iterationCount: '1'
+  })
+  //
+})()
