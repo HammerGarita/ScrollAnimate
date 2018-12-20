8f7fcda76e3862d43464d9a2b8fa0b6a0666a77a
Hammer Garita <36253181+HammerGarita@users.noreply.github.com>
2018-12-20 17:45:33 -0600

    Update scripts.js

--- example/js/scripts.js
+++ example/js/scripts.js
@@ -1,16 +1,14 @@
 (function () {
-  let element = document.querySelector('.scroll-horizontal-container')
-  element.addEventListener('wheel', (e) => {
-    if (e.deltaY !== 0) {
-      e.preventDefault()
-      window.requestAnimationFrame(() => {
-        element.scrollLeft += e.deltaY
-      })
-    }
-  })
+  // let element = document.querySelector('.scroll-horizontal-container')
+  // element.addEventListener('wheel', (e) => {
+  //   if (e.deltaY !== 0) {
+  //     e.preventDefault()
+  //     window.requestAnimationFrame(() => {
+  //       element.scrollLeft += e.deltaY
+  //     })
+  //   }
+  // })
 
-  ScrollAnimate.init({
-    scroll: 'horizontal'
-  })
+  ScrollFunny.init()
   //
 })()
